4681d06973a94a5d5d16c419c6e4ed44c3d9117c
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-18 14:20:26 +0100

    more tests

--- test/test_updated.py
+++ test/test_updated.py
@@ -3,5 +3,6 @@ import updated
 
 def test_updated():
     updated.get_pypi_version('matplotlib')
+    assert updated.needs_checking('matplotlib')
     updated.check_and_notify('matplotlib', '0.0.0')
     return
