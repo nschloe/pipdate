6eec237bf49bdbb3efa32bec55c4a7d7323b27ba
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-13 10:40:12 +0200

    version bump

--- pipdate/__about__.py
+++ pipdate/__about__.py
@@ -5,5 +5,5 @@ __author__ = u'Nico Schlömer'
 __email__ = 'nico.schloemer@gmail.com'
 __copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __email__)
 __license__ = 'License :: OSI Approved :: MIT License'
-__version__ = '0.2.0'
-__status__ = 'Development Status :: 3 - Alpha'
+__version__ = '0.2.1'
+__status__ = 'Development Status :: 4 - Beta'
