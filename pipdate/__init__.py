780557bd230832cbf6ac4714129f617636dd6873
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-13 11:31:26 +0200

    flake8 fixes

--- pipdate/__init__.py
+++ pipdate/__init__.py
@@ -5,5 +5,15 @@
 
 from .__about__ import __author__, __email__, __copyright__, __license__, __version__
 
-# pylint: disable=wildcard-import
-from .main import *
+from .main import needs_checking, get_pypi_version, check
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "needs_checking",
+    "get_pypi_version",
+    "check",
+]
