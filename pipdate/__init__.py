496544a8967347e5bc026d2a76a2d77ebefa0bba
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-22 21:59:28 +0200

    some fixes

--- pipdate/__init__.py
+++ pipdate/__init__.py
@@ -1,16 +1,7 @@
 """Check for updates on PyPi.
 """
 
-from .__about__ import __author__, __copyright__, __email__, __license__, __version__
+from .__about__ import __version__
 from .main import check, get_pypi_version, needs_checking
 
-__all__ = [
-    "__author__",
-    "__email__",
-    "__copyright__",
-    "__license__",
-    "__version__",
-    "needs_checking",
-    "get_pypi_version",
-    "check",
-]
+__all__ = ["__version__", "needs_checking", "get_pypi_version", "check"]
