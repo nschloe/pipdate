cf50a57a782cb535c889f11db2e2710c9d82df34
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-11 08:50:11 +0200

    pytest-codeblocks

--- tests/test_readme.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-# conda-forge doesn't have exdown
-# exdown = pytest.importorskip("exdown")
-import exdown
-
-this_dir = pathlib.Path(__file__).resolve().parent
-
-test_readme = exdown.pytests(this_dir / ".." / "README.md", syntax_filter="python")
